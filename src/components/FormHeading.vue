fa426a95dbfda85847d2c6387ef82a595b2b45a9
Irakli <irakli@tchigl.com>
2021-02-15 20:36:41 +0400

    added image

--- src/components/FormHeading.vue
+++ src/components/FormHeading.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <h1>Form Heading Join Us</h1>
+    <div class="imgAndHeading">
+      <img src="https://image.flaticon.com/icons/png/512/2037/2037710.png" />
+      <h1>Form Heading Join Us</h1>
+    </div>
     <p>
       There are many variations of passages of Lorem Ipsum available, but the
       majority have suffered alteration in some form, by injected humour.
@@ -16,8 +19,21 @@ export default {
 
 <style scoped>
 div {
-  width: 80%;
+  width: 100%;
   margin-left: auto;
   margin-right: auto;
+}
+img {
+  width: 20%;
+  height: 20%;
+  display: inline;
+}
+h1 {
+  display: inline;
+}
+.imgAndHeading {
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
